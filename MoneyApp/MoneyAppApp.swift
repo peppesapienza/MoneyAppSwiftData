f02c925034cf3b9e5c7cc45d83aa4d17b1999264
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2024-12-08 12:08:46 +1100

    Initial Commit

---
+++ MoneyApp/MoneyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyAppApp.swift
+//  MoneyApp
+//
+//  Created by Giuseppe Sapienza on 8/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoneyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
